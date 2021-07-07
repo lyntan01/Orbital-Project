576a422b43126cf2bfcf739b8ee2b6a26a98c78d
Ke Xin <80565451+kekekexinnn@users.noreply.github.com>
2021-07-07 20:58:32 +0800

    Delete Main.js

--- static/js/Main.js
+++
@@ -1,8 +0,0 @@
-function showComment(){
-    var x = document.getElementById("comment-area");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
